7778501b7fd7065997119a253bcd95072a4d44cc
lanetechmun <lanetechmun@gmail.com>
2026-02-19 19:33:11 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -207,3 +207,10 @@ function openFullImage(src) {
 document.addEventListener('DOMContentLoaded', () => {
     initTeamCarousel('team-moments');
 });
+
+const hamburger = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+hamburger.addEventListener('click', () => {
+  mobileMenu.style.display = mobileMenu.style.display === 'flex' ? 'none' : 'flex';
+});
